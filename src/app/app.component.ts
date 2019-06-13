954331a94324cb1065f4b13830fdb5e6a57b8881
Chris Jannenga <cjannenga312@gmail.com>
2019-06-13 16:33:20 -0700

    Added a TD Form and some css for invalid inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('demoForm', {static: true}) signupForm: NgForm
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
