e3ba4bd9d3aa11bf13eabfc779fbb669500875cb
Chris Jannenga <cjannenga312@gmail.com>
2019-06-14 10:35:34 -0700

    Completed the template driven form section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,13 +7,39 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('demoForm', {static: true}) signupForm: NgForm
+  @ViewChild('demoForm', {static: true}) signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  isSubmitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: `${suggestedName}@test.com`
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'Male'
+    });
   }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.isSubmitted = true;
+    this.signupForm.reset();
   }
 }
